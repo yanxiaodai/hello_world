d6b87eaac4e302b04aa7c921cd56b8cbd768710a
xywxyw123123@163.com <xywxyw123123@163.com>
2017-09-14 15:17:00 +0800

    initialize repostory

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+def hello
+  render html: "¡ hola,mundo!"
+end
+
+def goodbye
+  render html:"Goodbye,world!"
+end
+
+end
